5399f935b0cd2358c79557270df396e771f0e591
Dris777 <GolovchenkoR777@gmail.com>
2024-04-23 21:59:50 +0300

    add hero styles

--- test.js
+++ test.js
@@ -35,18 +35,11 @@
 // // },{})
 //
 //
-// console.log(tagsStats)
+const getSortedFriends = users => users.flatMap(({friends}) => friends).filter((name,index,array) => array.indexOf(name)===index).sort()
 
-function changeEven(numbers, value) {
-  // Пиши код ниже этой строки
-  let a = []
-   numbers.forEach( num => {
-     !(num%2)?a.push(num+value):a.push(num)
-
-  })
-  return a
-  // Пиши код выше этой строки
-}
+console.log(getSortedFriends([ { name: 'Moore Hensley', email: 'moorehensley@indexia.com', eyeColor: 'blue', friends: ['Sharron Pace'], isActive: false, balance: 2811, gender: 'male' }, { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com', eyeColor: 'blue', friends: ['Briana Decker', 'Sharron Pace'], isActive: true, balance: 3821, gender: 'female' }, { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com', eyeColor: 'green', friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'], isActive: false, balance: 3793, gender: 'male' }, { name: 'Elma Head', email: 'elmahead@omatom.com', eyeColor: 'green', friends: ['Goldie Gentry', 'Aisha Tran'], isActive: true, balance: 2278, gender: 'female' }, { name: 'Carey Barr', email: 'careybarr@nurali.com', eyeColor: 'blue', friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'], isActive: true, balance: 3951, gender: 'male' }, { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com', eyeColor: 'brown', friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'], isActive: false, balance: 1498, gender: 'male' }, { name: 'Sheree Anthony', email: 'shereeanthony@kog.com', eyeColor: 'brown', friends: ['Goldie Gentry', 'Briana Decker'], isActive: true, balance: 2764, gender: 'female' } ]))
+const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gender === gender).reduce((acc,user) => acc+=user.balance,0)
 
 
-console.log(changeEven([1, 2, 3, 4, 5], 10))
+
+console.log(getTotalBalanceByGender([ { name: 'Moore Hensley', email: 'moorehensley@indexia.com', eyeColor: 'blue', friends: ['Sharron Pace'], isActive: false, balance: 2811, gender: 'male' }, { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com', eyeColor: 'blue', friends: ['Briana Decker', 'Sharron Pace'], isActive: true, balance: 3821, gender: 'female' }, { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com', eyeColor: 'green', friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'], isActive: false, balance: 3793, gender: 'male' }, { name: 'Elma Head', email: 'elmahead@omatom.com', eyeColor: 'green', friends: ['Goldie Gentry', 'Aisha Tran'], isActive: true, balance: 2278, gender: 'female' }, { name: 'Carey Barr', email: 'careybarr@nurali.com', eyeColor: 'blue', friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'], isActive: true, balance: 3951, gender: 'male' }, { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com', eyeColor: 'brown', friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'], isActive: false, balance: 1498, gender: 'male' }, { name: 'Sheree Anthony', email: 'shereeanthony@kog.com', eyeColor: 'brown', friends: ['Goldie Gentry', 'Briana Decker'], isActive: true, balance: 2764, gender: 'female' } ],'male'))
